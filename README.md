de3335845ea3ec6ef385cbb4c5854e064c26d618
Claudiu Birlean <claudiu.birlean@fortech.ro>
2025-03-04 08:31:22 +0200

    initial work

--- README.md
+++ README.md
@@ -1,70 +1,46 @@
-# Getting Started with Create React App
+# Material UI - Create React App example with styled-components
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## How to use
 
-## Available Scripts
+Download the example [or clone the repo](https://github.com/mui/material-ui):
 
-In the project directory, you can run:
+<!-- #repo-reference -->
 
-### `yarn start`
+```bash
+curl https://codeload.github.com/mui/material-ui/tar.gz/master | tar -xz --strip=2 material-ui-master/examples/material-ui-cra-styled-components
+cd material-ui-cra-styled-components
+```
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Install it and run:
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+```bash
+npm install
+npm start
+```
 
-### `yarn test`
+## CodeSandbox
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+<!-- #repo-reference -->
 
-### `yarn build`
+Note that CodeSandbox is not supporting react-app-rewired, yet you can [still see the code](https://codesandbox.io/p/sandbox/github/mui/material-ui/tree/master/examples/material-ui-cra-styled-components).
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+<!-- #host-reference -->
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+The following link leverages this demo: https://next.mui.com/material-ui/integrations/interoperability/#change-the-default-styled-engine with Parcel's alias feature within the `package.json`.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+[![Edit on CodeSandbox](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/p/sandbox/styled-components-interoperability-w9z9d)
 
-### `yarn eject`
+## The idea behind the example
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+This example demonstrates how to set up Material UI with [Create React App](https://github.com/facebookincubator/create-react-app), using [styled-components](https://styled-components.com/) as a style engine for your application.
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+## Versions compatibility
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+Note, the version 5 of `@mui/styled-engine-sc` is compatible with version 5 of `styled-components`, while the version 6 of `@mui/styled-engine-sc` (currently in alpha), is compatible with v6 of `styled-components`. When incorporating these dependencies into your project, consider this compatibility requirement. In this example application, both libraries are using version 6.
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+## What's next?
 
-## Learn More
+<!-- #host-reference -->
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+You now have a working example project.
+You can head back to the documentation and continue by browsing the [templates](https://next.mui.com/material-ui/getting-started/templates/) section.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,178 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import ProTip from "./ProTip";
+import Countdown from "react-countdown";
+import simplyCountdown from "simplycountdown.js";
+import styled from "styled-components";
 
-function App() {
+// This is an example with default parameters
+const contdown = simplyCountdown("#wedding-countdown", {
+  year: 2025, // Target year (required)
+  month: 6, // Target month [1-12] (required)
+  day: 28, // Target day [1-31] (required)
+  hours: 0, // Target hour [0-23], default: 0
+  minutes: 0, // Target minute [0-59], default: 0
+  seconds: 0, // Target second [0-59], default: 0
+  words: {
+    // Custom labels, with lambda for plurals
+    days: { root: "day", lambda: (root, n) => (n > 1 ? root + "s" : root) },
+    hours: { root: "hour", lambda: (root, n) => (n > 1 ? root + "s" : root) },
+    minutes: {
+      root: "minute",
+      lambda: (root, n) => (n > 1 ? root + "s" : root),
+    },
+    seconds: {
+      root: "second",
+      lambda: (root, n) => (n > 1 ? root + "s" : root),
+    },
+  },
+  plural: true, // Use plurals for labels
+  inline: false, // Inline format: e.g., "24 days, 4 hours, 2 minutes"
+  inlineSeparator: ", ", // Separator for inline format, default: ", "
+  inlineClass: "simply-countdown-inline", // CSS class for inline countdown
+  enableUtc: false, // Use UTC time if true
+  refresh: 1000, // Refresh interval in ms, default: 1000
+  sectionClass: "simply-section", // CSS class for each countdown section
+  amountClass: "simply-amount", // CSS class for numeric values
+  wordClass: "simply-word", // CSS class for unit labels
+  zeroPad: false, // Pad numbers with leading zero
+  removeZeroUnits: false, // Remove units with zero value
+  countUp: false, // Count up after reaching zero
+  onEnd: () => {}, // Callback when countdown ends
+  onStop: () => {}, // Callback when countdown is stopped
+  onResume: () => {}, // Callback when countdown is resumed
+  onUpdate: (params) => {}, // Callback when countdown is updated
+});
+
+function Copyright() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography
+      variant="body2"
+      align="center"
+      sx={{
+        color: "text.secondary",
+      }}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}.
+    </Typography>
   );
 }
 
-export default App;
+const Navigation = () => {
+  return (
+    <Container style={{ padding: "40px 0px" }}>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" component="h1" color="white">
+          Ne casatorim!
+        </Typography>
+        <nav style={{ display: "flex", gap: "24px", alignItems: "center" }}>
+          <Link>MOMENTUL SI LOCUL</Link>
+          <Link>Confirmare</Link>
+        </nav>
+      </Box>
+    </Container>
+  );
+};
+
+const MyCountdown = (props) => {
+  return (
+    <>
+      <span>Salut</span>
+    </>
+  );
+};
+
+const Circle = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+  width: 100px;
+  height: 100px;
+  background: rgba(241, 78, 149, 0.8);
+  margin: 0 4px;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  -ms-border-radius: 50%;
+  border-radius: 50%;
+  position: relative;
+`;
+
+const Header = () => {
+  return (
+    <Box
+      style={{
+        backgroundPosition: "0px 0px",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundImage:
+          "url(https://i0.wp.com/photofocus.com/wp-content/uploads/2021/03/wedding-hero-silhouette.jpg?w=2560&ssl=1)",
+      }}
+      height="900px"
+      display="flex"
+      flexDirection="column"
+    >
+      <Navigation></Navigation>
+      <Countdown
+        date={1755957600000}
+        daysInHours={false}
+        renderer={(props) => {
+          return (
+            <div
+              style={{
+                display: "flex",
+                gap: "12px",
+                width: "100%",
+                height: "70%",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Circle>{props.days} zile</Circle>
+              <Circle>{props.hours} ore</Circle>
+              <Circle>{props.minutes} minute</Circle>
+              <Circle>{props.seconds} secunde</Circle>
+            </div>
+          );
+        }}
+      ></Countdown>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <StyledButton variant="outlined" href="https://google.com">
+          Salvati Data
+        </StyledButton>
+      </div>
+    </Box>
+  );
+};
+const StyledButton = styled.a`
+    padding: 15px 20px;
+    background: #fff !important;
+    color: #F14E95;
+    border: none !important;
+    font-size: 14px;
+    text-transform: uppercase;
+    border-radius: 30px;
+    text-decoration: none;
+}`;
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Copyright />
+    </>
+  );
+}
